60c6a2d63dedc699ee7e4c8aa122f346dce94ea0
chenlinqiang <327371999@qq.com>
2020-01-31 22:36:04 +0800

    first commit

---
+++ eclipse-workspace/DemoArrayList/src/arrayList/Demo01.java
@@ -0,0 +1,15 @@
+package arrayList;
+
+import java.util.*;
+
+public class Demo01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 
+		ArrayList al = new ArrayList();
+		//
+		System.out.println("size:"+al.size());
+	}
+
+}
